cb8216792e43b4456fe2a03420d46e2eb2465b05
Bamidele <fisiwizy@gmail.com>
2017-05-26 11:41:30 +0100

    test  setup with helloworld

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+  <div>Hello World</div>,
+  document.getElementById('root')
+);
