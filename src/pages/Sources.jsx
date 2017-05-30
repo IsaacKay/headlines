934d0529fadcf954621b91c8d81b7f0812e22337
Bamidele <fisiwizy@gmail.com>
2017-05-30 23:37:28 +0100

    Implement realtime search for Sources component

--- src/pages/Sources.jsx
+++ src/pages/Sources.jsx
@@ -12,6 +12,8 @@ export default class Sources extends React.Component {
    */
   constructor() {
     super();
+    this.handleSearchSource = this.handleSearchSource.bind(this);
+    this.searchSource = this.searchSource.bind(this);
     this.state = { sources: [] };
   }
   /**
@@ -45,6 +47,42 @@ export default class Sources extends React.Component {
     return sources;
   }
   /**
+   * @description searches through sources name. Creates new state
+   * with search result.
+   * set component's state to search result
+   * search is implemented through javascript regular expression
+   * @param {string} searchTerm - Search Terms
+   * @param {object[]} sources - Array of news sources
+   * @returns {void}
+   */
+  searchSource(searchTerm, sources) {
+    // change searchTerm to regular expression
+    searchTerm = new RegExp(searchTerm, 'i');
+    const searchResult = sources.filter((source) => {
+      // get rid of special characters in sourceName
+      const sourceName = source.name.replace(/[^a-z0-9\s]/ig, '');
+      const isSearchTermPresent = searchTerm.test(sourceName);
+      return isSearchTermPresent;
+    });
+    this.setState({ sources: searchResult });
+  }
+  /**
+   * @description - Gets all news sources if nothing is searched.
+   * Searches through source and sets this.state.sources to search result;
+   * @param {Event} event - javascript event
+   * @returns {void}
+   */
+  handleSearchSource(event) {
+    const searchTerm = event.target.value;
+    const sources = this.state.sources;
+    // if sources is empty or nothing is searched
+    if (sources.length < 1 || !searchTerm) {
+      this.getAllNewsSources();
+    } else {
+      this.searchSource(searchTerm, sources);
+    }
+  }
+  /**
    * @description Renders an array of sources
    * @return {JSX.Element} JSDoc containing News Sources
    */
@@ -52,6 +90,7 @@ export default class Sources extends React.Component {
     const sources = this.makeActualSourcesComponents();
     return (
       <div>
+        <input id="search" type="text" onChange={this.handleSearchSource} />
         {sources}
       </div>
     );
