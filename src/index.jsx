43b3d4b6ad0577f87ee3d46b145dc11d543d84f9
Bamidele <fisiwizy@gmail.com>
2017-05-31 02:20:05 +0100

    implement route

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import Articles from './pages/Articles.jsx';
+import Sources from './pages/Sources.jsx';
 
 ReactDom.render(
-  <div>Hello World</div>,
-  document.getElementById('root')
+  <Router history={hashHistory}>
+    <IndexRoute component={Sources} />
+    <Route path="sources" component={Sources} />
+    <Route path="articles" component={Articles} />
+  </Router>
 );
