d06add0d7250e827f30084982cce1b366415aff8
Bamidele <fisiwizy@gmail.com>
2017-05-31 02:45:35 +0100

    Fix route leading to sources only bug

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Articles from './pages/Articles.jsx';
 import Sources from './pages/Sources.jsx';
 
 ReactDom.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Sources}>
+    <Route path="/">
+      <IndexRoute component={Sources} />
       <Route path="sources" component={Sources} />
       <Route path="articles" component={Articles} />
     </Route>
