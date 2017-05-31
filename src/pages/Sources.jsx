3c577f92b8ef75e342ff828e10dbdb618e2e427a
Bamidele <fisiwizy@gmail.com>
2017-05-31 02:32:43 +0100

    Fix route bug

--- src/pages/Sources.jsx
+++ src/pages/Sources.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Articles extends React.Component {
   render() {
     return (<div>
-      <h1> Articles</h1>
+      <h1> Sources</h1>
       <h3> Still under dev</h3>
     </div>);
   }
