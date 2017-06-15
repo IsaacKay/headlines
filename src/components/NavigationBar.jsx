ad4a607d1500ac37d41213f774012324fa966221
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-06-15 08:59:26 +0100

    feature: User Authentication
    implement user authentication using react-google-login
    implement signout

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,25 +1,57 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 import { Link } from 'react-router';
 
-const { Nav, Navbar, NavItem } = ReactBootstrap;
-const NavigationBar = (() => {
-  const navBar = (
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">International Aproko</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-            <NavItem eventKey={1}><Link to="sources">Sources</Link></NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-  return navBar;
-});
-export default NavigationBar;
+const { Nav, Navbar, NavItem, Button } = ReactBootstrap;
+
+/**
+ * @description Navigation Component used across all components
+ */
+export default class NavigationBar extends React.Component {
+  /**
+   * @description -Logs out user when the logout button is clicked
+   * @returns {void}
+   */
+  static logout() {
+    localStorage.removeItem('user');
+    location.reload();
+  }
+
+  /**
+   * @returns {JSX.Element} Navbar element fro ReactBootstrap
+   */
+  render() {
+    const isPathSources = !!this.props.location.pathname.match(/\/sources/);
+    let sourceClass = '';
+    if (isPathSources) {
+      sourceClass = 'hidden';
+    } else {
+      sourceClass = '';
+    }
+    const navBar = (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">International Aproko</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem className={sourceClass} eventKey={1}>
+              <Link to="sources">Sources</Link>
+            </NavItem>
+          </Nav>
+          <Nav className="pull-right">
+            <NavItem eventKey={2}>
+              <Button bsStyle="primary" bsSize="xs"
+                onClick={NavigationBar.logout}>Logout
+              </Button>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+    return navBar;
+  }
+}
